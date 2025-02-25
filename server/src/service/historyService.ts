68ed6e86df283e46abaedeb33f93ca08e790cb14
Ryan Gayle <gayler02@nyu.edu>
2025-02-25 18:30:37 -0500

    updated searchHistory json file

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -45,7 +45,9 @@ class HistoryService {
   }
   }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  async addCity(city: string) {}
+  async addCity(city: string) {
+
+  }
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   async removeCity(id: string) {}
 }
